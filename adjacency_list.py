c035d02916756ea2b6da62037c7696c00ca5f568
Lucas Spagnolo Bombana <lucasspagnolobombana@gmail.com>
2021-10-19 18:00:05 -0300

    Criando estruturas de dados para armazenar o grafo do problema

---
+++ adjacency_list.py
@@ -0,0 +1,27 @@
+class Graph:
+
+    def __init__(self, vertexes):
+        self.vertexes = vertexes
+        self.graph = [[] for i in range(self.vertexes)]
+
+    def add_edge(self, u, v):
+        self.graph[u - 1].append(v)
+        self.graph[v - 1].append(u)
+
+    def show_list(self):
+        for i in range(self.vertexes):
+            print(f'{ i + 1}: ', end='  ')
+            text_to_be_printed = ''
+            for j in self.graph[i]:
+                text_to_be_printed += str(j) + '  ->  '
+            text_to_be_printed += 'None'
+            print(text_to_be_printed)
+
+g = Graph(4)
+
+g.add_edge(1, 2)
+g.add_edge(1, 3)
+g.add_edge(1, 4)
+g.add_edge(2, 3)
+
+g.show_list()
