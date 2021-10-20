b06944b2b66930e760c359fa1efb7fc5265bbe98
Lucas Spagnolo Bombana <lucasspagnolobombana@gmail.com>
2021-10-20 10:49:18 -0300

    Criando funcionalidades: get_deggre, has_edge_between e get_edge_deggre

--- adjacency_list.py
+++ adjacency_list.py
@@ -17,8 +17,20 @@ class Graph:
             text_to_be_printed += 'None'
             print(text_to_be_printed)
 
+    def get_deggre(self, vertex: int) -> int:
+        return len(self.graph[vertex - 1])
+
+    def has_edge_between(self, vertex_u: int, vertex_v: int) -> bool:
+        return (vertex_v) in self.graph[vertex_u - 1]
+
+    def get_edge_deggre(self, vertex_u: int, vertex_v: int) -> float or bool:
+        if(self.has_edge_between(vertex_u, vertex_v)):
+            return (self.get_deggre(vertex_u) + self.get_deggre(vertex_v)) / 2
+        else:
+            return False
+
 '''
-Exemplo de uso:
+#Exemplo de uso:
 
 g = Graph(4)
 
@@ -27,5 +39,9 @@ g.add_edge(1, 3)
 g.add_edge(1, 4)
 g.add_edge(2, 3)
 
+#print(g.get_deggre(1))
+#print(g.has_edge_between(1, 1))
+#print(g.has_edge_between(1, 2))
+#print(g.get_edge_deggre(1, 1))
 g.show_list()
 '''
